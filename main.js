9ef7a4a41c434e0711bc731a519b7e94de0e66aa
raulbborba19 <raul.borba@escola.pr.gov.br>
2025-09-25 22:17:41 -0300

    .

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 const vamp = document.querySelector(".vamp")
-const startButton = document.querySelector(".start")
 const gameOverScreen = document.querySelector(".game-over")
 const scoreElement = document.querySelector(".score")
+const gameBoard = document.querySelector(".game-board")
 
 audioStart = new Audio("");
 const gameOverSound = new Audio("");
 
 let gameStarted = false;
-let birdPosition = 200;
-let birdVelocity = 0;
-const gravity = -0.5;
+let score = 0;
+let vampPosition = 200;
+let vampVelocity = 0;
+const gravity = -0.6;
 const lift = 10; 
+const vampWidth = 60;
+const boardHeight = 500;
 
 const startGame = () => {
     gameStarted = true;
     audioStart.play();
 
     vamp.style.opacity = "1";
-    stake.style.opacity = "1";
+}
+
+const vampJump = () => {
+    if (!gameStarted) {
+        startGame(); 
+    }
+    vampVelocity = lift;
 }
 
 const updateScore = () => {
@@ -27,33 +36,11 @@ const updateScore = () => {
     stake.style.animation = `stake-animation ${animationSpeed}s infinite linear`; 
 }
 
-const loop = setInterval(() => {
-const stakePosition = stake.offsetLeft;
-const vampPosition = +window.getComputedStyle(vamp).bottom.replace("px", "");
-
-    if(stakePosition <= 120 && stakePosition > 0 && vampPosition < 80) {
-        stake.style.animation = "none";
-        stake.style.left = `${stakePosition}px`;
-
-        vamp.style.animation = "none";
-        vamp.style.left = `${marioPosition}px`;
-
-        vamp.src = "./img/game-over.png";
-        vamp.style.width = "75px";
-        vamp.style.marginLeft = "50px";
-
-        audioStart.pause();
-        gameOverSound.play();
-
-        clearInterval(loop);
-        gameOverScreen.style.display = "flex";
-    } else if (stakePosition < 0 && gameStard) {
-        updateScore();
-        stake.style.left = '';
+document.addEventListener("keydown", (e) => {
+    if (e.key === " " || e.key === "ArrowUp") { 
+        vampJump();
     }
-},10);
-document.addEventListener("keydown", jump);
-
+});
 
 const restartGame = () => {
     window.location.reload();
